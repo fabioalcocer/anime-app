d3c94c17f78ee47c6bb4af7c3bf31070c5e5afef
fabio alcocer <fabioalcocersejas17@gmail.com>
2022-08-06 00:46:02 -0400

    add tailwind

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,11 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <div className="App">
-      <h1>Anime app</h1>
+      <h1 className="text-3xl font-bold underline">Anime app</h1>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
