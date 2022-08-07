4e1776fd93e85d527034829e29baa1d6a5ae4945
fabio alcocer <fabioalcocersejas17@gmail.com>
2022-08-07 15:46:33 -0400

    update footer and README

--- README.md
+++ README.md
@@ -1,1 +1,51 @@
-* Modal para el view more y cambiar dinamicamente la cancion a reproducir
+# ANIME APP PROJECT
+
+## Tabla de Contenidos
+
+- [Preview](#preview)
+  - [Links](#links)
+- [Proceso](#proceso)
+  - [Construido con](#construido-con)
+  - [Recursos Utilizados](#recursos-utilizados)
+- [Autor](#Autor)
+
+## Preview
+
+### Screenshot
+
+![](./src/assets/preview.png)
+
+## Proyecto
+
+Este proyecto fue realizado con la participación de los integrantes de OpenBootcamp, y es parte de una dinámica llamada "Weekend apps"
+
+### Links
+
+- Repositorio: [Solution](https://github.com/fabioalcocer/anime-app.git)
+- Live Site URL: [Live App](https://anime-app-ob.netlify.app/)
+
+## Proceso
+
+### Construido con:
+
+- Mobile-first workflow
+- Responsive Design
+- Flex Layout
+- React
+- Tailwind
+
+### Recursos Utilizados
+
+- [Tailwind](https://tailwindcss.com/docs/)
+- [Cloudinary](https://cloudinary.com/)
+- [Audio Web API's](https://developer.mozilla.org/en-US/docs/Web/API/HTMLAudioElement/Audio)
+- [MDN Web Docs](https://developer.mozilla.org/es/docs/Web)
+
+## Autor
+
+### OpenBootcamp Community
+
+- Bootcamp - [OpenBootcamp](https://bit.ly/3E5AGBt)
+- Twitter - [@Open_Bootcamp](https://twitter.com/Open_Bootcamp)
+- LinkedIn - [@Open_Bootcamp](https://www.linkedin.com/school/openbootcamp-escuela/)
+- GitHub - [@fabioalcocer](https://github.com/fabioalcocer/)
--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,45 +1,49 @@
 import React from "react";
-import { FiGithub, FiInstagram, FiLinkedin, FiTwitter } from "react-icons/fi";
+import { BsInstagram, BsGithub, BsLinkedin, BsTwitter } from "react-icons/bs";
 
 function Footer() {
   return (
     <footer className="container min-w-full bg-gray-900 text-center text-white">
-      <div className="flex justify-center gap-2 p-2">
+      <div className="flex justify-center gap-5 p-2">
         <a
           href="https://twitter.com/Open_Bootcamp"
+          target="_blank"
           type="button"
-          className="m-1 h-9 w-9 rounded-full border-2 border-white uppercase leading-normal text-white transition duration-150 ease-in-out hover:bg-black hover:bg-opacity-5 focus:outline-none focus:ring-0"
+          className="m-1 text-white transition duration-150 ease-in-out focus:outline-none focus:ring-0"
         >
-          <FiTwitter className="mx-auto h-full w-4" />
+          <BsTwitter className="transition-colors: mx-auto text-2xl text-slate-300 duration-300 hover:text-white" />
         </a>
 
         <a
           href="https://www.instagram.com/openbootcamp_/"
+          target="_blank"
           type="button"
-          className="m-1 h-9 w-9 rounded-full border-2 border-white uppercase leading-normal text-white transition duration-150 ease-in-out hover:bg-black hover:bg-opacity-5 focus:outline-none focus:ring-0"
+          className="m-1 text-white transition duration-150 ease-in-out focus:outline-none focus:ring-0"
         >
-          <FiInstagram className="mx-auto h-full w-4" />
+          <BsInstagram className="transition-colors: mx-auto text-2xl text-slate-300 duration-300 hover:text-white" />
         </a>
 
         <a
           href="https://www.linkedin.com/school/openbootcamp-escuela/"
+          target="_blank"
           type="button"
-          className="m-1 h-9 w-9 rounded-full border-2 border-white uppercase leading-normal text-white transition duration-150 ease-in-out hover:bg-black hover:bg-opacity-5 focus:outline-none focus:ring-0"
+          className="m-1 text-white transition duration-150 ease-in-out focus:outline-none focus:ring-0"
         >
-          <FiLinkedin className="mx-auto h-full w-4" />
+          <BsLinkedin className="transition-colors: mx-auto text-2xl text-slate-300 duration-300 hover:text-white" />
         </a>
 
         <a
           href="https://github.com/fabioalcocer/anime-app.git"
+          target="_blank"
           type="button"
-          className="m-1 h-9 w-9 rounded-full border-2 border-white uppercase leading-normal text-white transition duration-150 ease-in-out hover:bg-black hover:bg-opacity-5 focus:outline-none focus:ring-0"
+          className="m-1 text-white transition duration-150 ease-in-out focus:outline-none focus:ring-0"
         >
-          <FiGithub className="mx-auto h-full w-4" />
+          <BsGithub className="transition-colors: mx-auto text-2xl text-slate-300 duration-300 hover:text-white" />
         </a>
       </div>
 
       <div className="bg-gray-800 p-4 text-center">
-        <a className="text-sm" href="https://bit.ly/3E5AGBt">
+        <a className="text-sm text-slate-400" href="https://bit.ly/3E5AGBt">
           © 2022 OpenBootcamp Community™. All Rights Reserved.
         </a>
       </div>
