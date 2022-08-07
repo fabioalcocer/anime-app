092f65f730cca0abb09566ee52f73e87d39d1954
fabio alcocer <fabioalcocersejas17@gmail.com>
2022-08-06 21:19:27 -0400

    add sticky header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,12 +3,12 @@ import Logo from "../assets/logo.png";
 function header() {
   return (
     <>
-      <div className=" bg-gray-900 py-3 px-4 text-center text-xl font-bold uppercase">
+      <header className=" bg-gray-900 py-3 px-4 text-center text-xl font-bold uppercase sticky top-0 md:static">
         <div className="mx-auto flex max-w-5xl items-center justify-between">
           <img className="h-14 w-20" src={Logo} alt="logo" />
           <h1>Anime App</h1>
         </div>
-      </div>
+      </header>
     </>
   );
 }
