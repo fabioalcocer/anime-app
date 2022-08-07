5425bf06774733f27370a896a25c755cdde974e1
fabio alcocer <fabioalcocersejas17@gmail.com>
2022-08-06 23:25:14 -0400

    add key for cards

--- src/components/CardsContainer.jsx
+++ src/components/CardsContainer.jsx
@@ -1,11 +1,11 @@
 import Card from "./Card";
-import {data} from "../services/data"
+import { data } from "../services/data";
 
 function CardsContainer() {
   return (
     <section className="my-6 mx-auto flex max-w-7xl flex-wrap items-center justify-center gap-7 py-5 px-3">
       {data.map((card) => (
-        <Card title={card.name} img={card.imageSrc} />
+        <Card key={card.id} title={card.name} img={card.imageSrc} />
       ))}
     </section>
   );
--- src/services/data.js
+++ src/services/data.js
@@ -1,30 +1,36 @@
 export const data = [
   {
+    id: "1",
     name: "Shingeki no Kyojin",
     imageSrc:
       "https://res.cloudinary.com/daobmfotr/image/upload/v1659840620/anime-imgs/snk-banner_fzs10s.webp",
   },
   {
+    id: "2",
     name: "Shingeki no Kyojin",
     imageSrc:
       "https://res.cloudinary.com/daobmfotr/image/upload/v1659840620/anime-imgs/deathnote-banner_ehtkig.webp",
   },
   {
+    id: "3",
     name: "Shingeki no Kyojin",
     imageSrc:
       "https://res.cloudinary.com/daobmfotr/image/upload/v1659840620/anime-imgs/tokyo-banner_co9wwt.webp",
   },
   {
+    id: "4",
     name: "Shingeki no Kyojin",
     imageSrc:
       "https://res.cloudinary.com/daobmfotr/image/upload/v1659840619/anime-imgs/tg-banner_kx24xr.webp",
   },
   {
+    id: "5",
     name: "Shingeki no Kyojin",
     imageSrc:
       "https://res.cloudinary.com/daobmfotr/image/upload/v1659840619/anime-imgs/jk-banner_sd3z8i.webp",
   },
   {
+    id: "6",
     name: "Shingeki no Kyojin",
     imageSrc:
       "https://res.cloudinary.com/daobmfotr/image/upload/v1659840620/anime-imgs/kny-banner_j1y077.webp",
