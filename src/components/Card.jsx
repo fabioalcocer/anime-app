8d49f9642ebec79ddc2a97dc9a906aa567730d48
fabio alcocer <fabioalcocersejas17@gmail.com>
2022-08-06 23:48:37 -0400

    fix cards

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -2,19 +2,22 @@ import ViewBtn from "./ViewBtn";
 
 function Card({ title, img }) {
   return (
-    <div className="flex h-32 w-full max-w-lg flex-col overflow-hidden rounded-md bg-cover bg-no-repeat contrast-125 sm:h-36 md:h-44">
-      <img src={img} className="absolute top-0 left-0 bottom-0 right-0 z-0" />
+    <div className="flex h-48 w-full max-w-lg flex-col overflow-hidden rounded-md contrast-125 sm:h-40 md:h-48">
+      <img
+        src={img}
+        className="absolute top-0 left-0 bottom-0 right-0 z-0 h-full w-full object-cover"
+      />
       <p className="p-5 text-lg font-bold text-white drop-shadow-3xl">
         {title}
       </p>
-      <div className="mt-auto flex h-[40%] w-full translate-y-1 items-center justify-between rounded-sm bg-black/30 px-4 backdrop-blur-sm transition-transform duration-500">
-        <h3 className="font-semibold text-red-100 drop-shadow-3xl">
+      <div className="mt-auto flex h-1/2 w-full translate-y-1 flex-col justify-center gap-4 rounded-sm bg-black/30 px-4 backdrop-blur-sm transition-transform duration-500 sm:h-[40%] sm:flex-row sm:items-center sm:justify-between">
+        <h3 className="text-base font-semibold text-red-100 drop-shadow-3xl md:text-lg">
           Guren No Yumiya OP.1
         </h3>
-        <div className="flex items-center gap-5">
+        <div className="flex items-center gap-3 sm:gap-5">
           <ViewBtn />
-          <button className="group relative inline-flex items-center justify-center overflow-hidden rounded-lg bg-gradient-to-br from-pink-500 to-orange-400 p-0.5 text-sm font-bold text-gray-900 hover:text-white focus:outline-none focus:ring-4 focus:ring-pink-200 group-hover:from-pink-500 group-hover:to-orange-400 dark:text-white dark:focus:ring-pink-800">
-            <span className="relative rounded-md bg-white px-5 py-2.5 transition-all duration-75 ease-in group-hover:bg-opacity-0 dark:bg-gray-900">
+          <button className="group relative inline-flex items-center justify-center overflow-hidden rounded-lg bg-gradient-to-br from-pink-500 to-orange-400 p-0.5 text-sm font-bold text-gray-900 hover:text-white focus:outline-none group-hover:from-pink-500 group-hover:to-orange-400 dark:text-white">
+            <span className="relative rounded-md bg-white py-[6px] px-2 transition-all duration-75 ease-in group-hover:bg-opacity-0 dark:bg-gray-900">
               View More
             </span>
           </button>
--- src/components/ViewBtn.jsx
+++ src/components/ViewBtn.jsx
@@ -3,7 +3,7 @@ import { FiPlayCircle } from "react-icons/fi";
 function ViewBtn() {
   return (
     <button className="group relative inline-flex items-center justify-center overflow-hidden rounded-lg bg-gradient-to-br from-green-400 to-blue-600 p-0.5 font-medium text-gray-900 hover:text-white focus:outline-none group-hover:from-green-400 group-hover:to-blue-600 dark:text-white dark:focus:ring-green-800">
-      <span className="relative flex gap-2 rounded-md bg-white p-2 transition-all duration-75 ease-in group-hover:bg-opacity-0 dark:bg-gray-900">
+      <span className="relative flex gap-2 rounded-md bg-white py-1 px-2 transition-all duration-75 ease-in group-hover:bg-opacity-0 dark:bg-gray-900">
         <FiPlayCircle className="text-2xl"/>
         Play
       </span>
