75ae18d2c39003a0d5202d75a3e3da9d34b2a267
fabio alcocer <fabioalcocersejas17@gmail.com>
2022-08-10 00:34:39 -0400

    change styles for modal

--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -3,7 +3,7 @@ import { FiX, FiMusic } from "react-icons/fi";
 function Modal({ modalRef, filterOpening, card }) {
   return (
     <dialog
-      className="fixed top-1/2 left-1/2 w-1/3 min-w-[298px] max-w-xs -translate-x-1/2 -translate-y-1/2 rounded-lg bg-slate-800 py-4 px-7 backdrop:bg-black/50 sm:backdrop:backdrop-blur-sm"
+      className="fixed top-1/2 left-1/2 w-1/3 min-w-[298px] max-w-xs -translate-x-1/2 -translate-y-1/2 rounded-lg bg-slate-800 py-4 px-5 backdrop:bg-black/50 sm:backdrop:backdrop-blur-sm"
       ref={modalRef}
     >
       <div className="flex items-center justify-between">
@@ -23,7 +23,7 @@ function Modal({ modalRef, filterOpening, card }) {
             onClick={() => {
               filterOpening(op[Object.keys(op)[1]]);
             }}
-            className="inline-flex cursor-pointer items-center border-b-[1px] border-slate-300 border-opacity-20 pb-2 text-sm font-bold uppercase hover:text-orange-400"
+            className="transition-colors: inline-flex cursor-pointer items-center rounded-lg bg-slate-700 py-2 px-2 text-sm font-bold uppercase duration-300 hover:bg-slate-600 hover:text-orange-400"
           >
             <FiMusic className="mr-4 text-xl text-purple-400" />
             {`${op.title} ${Object.keys(op)[1]}`}
